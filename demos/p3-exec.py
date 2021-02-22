a70048a6b2210cc5dc7acd5ea4209a3b041b7191
mpolague <mpolague@miners.utep.edu>
2021-02-21 20:59:21 -0700

    word count works

--- demos/p3-exec.py
+++ demos/p3-exec.py
@@ -3,10 +3,13 @@ import os, sys, time, re
 from os.path import isfile, join
 prompt = "$ "
 
+inp = input("please enter something before proceeding")
+print(inp)
+print(os.pipe())
+
+
 pid = os.getpid() # setting the pid
-
 os.write(1, ("About to fork (pid:%d)\n" % pid).encode()) # forks
-
 rc = os.fork() # forks 
 
 # handler if forking execution failed
@@ -16,12 +19,39 @@ if rc < 0:
 
 # if the forking was successful, it outputs both child's and parent's pid
 elif rc == 0:                   # child
+    os.write(1, ("Child: My pid==%d.  Parent's pid=%d\n" % 
+                 (os.getpid(), pid)).encode())
+
+    # ---- PIPELINING -----
+    def pipe():
+        print(os.pipe())
+    
     # ---- lists files in CWD ----
-    def list_files():
-        listoffiles = os.listdir('.')
-        for file in listoffiles:
-            print(" ",file)
+    def list_files(command):
+        if len(command) == 3:
+            if command[1] == '>':
+                os.close(1) #redirect child's stdout
+                os.open(command[2], os.O_CREAT | os.O_WRONLY);
+                os.set_inheritable(1, True)
+                listoffiles = os.listdir('.')
+                for file in listoffiles:
+                    print(" ",file)
+                return
+            else:
+                print("ls: No such file or directory, command not recognized.")
+                return
+            
+        elif len(command) == 1:
+            listoffiles = os.listdir('.')
+            for file in listoffiles:
+                print(" ",file)
+
+        
+        else:
+            print("ls: No such file or directory, command not recognized.")
+            return
         return
+    
     # ---- prints CWD -----
     def getcwd():
         print(" ",os.getcwd())
@@ -31,6 +61,15 @@ elif rc == 0:                   # child
         comm = input()
         return comm
 
+    # ---- prints out contents of a file ----
+    def cat():
+        print(os.cat(test3.txt))
+
+    #----- prints word count of a file ----
+    def wc(command):
+        file = command[1]
+        print(os.system("wc " + file))
+        
     # ---- change directory ----
     def cd(command):
         new_dir = command[1]
@@ -45,34 +84,7 @@ elif rc == 0:                   # child
                 print("")
                 print("Can't change the Current Working Directory, dir does not exist!!!!")
                     
-        getcwd()
-    #----------------------------------------------------------------------------------
-    # Helper to recognize if the command entered by the user is legal or illegal
-    # -- if returns 0 = keep askig for user input
-    # -- if returns 1 = exit program
-    def recognize_command(tokenized_string):
-        # recognizes commands such as 'ls', 'cd', 'exit'
-        if tokenized_string[0] == 'ls':
-            if len(tokenized_string) > 1:
-                print("ls: No such file or directory, command not recognized.")
-                return
-            list_files()
-            return 0
-        
-        elif tokenized_string[0] == "exit": #My shell terminates
-            return 1
-        
-        #--------- CD ------------
-        elif tokenized_string[0] == "cd":
-            cd(tokenized_string)
-        #--------- CWD ------------
-        elif tokenized_string[0] == "cwd":
-            getcwd()
-            
-        else:
-            print("Command: entered not recognized.")
-            return
-            
+        getcwd()            
 
     #----------------------------------------------------------------------------------
     # will tokenize the command entered and the first word will be "ls", which will list
@@ -80,9 +92,6 @@ elif rc == 0:                   # child
     def tokenize(command):
         tokenized_string = command.split()
         return tokenized_string
-    #----------------------------------------------------------------------------------
-    os.write(1, ("Child: My pid==%d.  Parent's pid=%d\n" % 
-                 (os.getpid(), pid)).encode())
     #----------------------------------------------------------------------------------
     while 1:
         if 'PS1' in os.environ:
@@ -95,12 +104,18 @@ elif rc == 0:                   # child
 
         #--------- CHECKS LENGTH OF INPUT -------
         if len(command) == 0: break #done if nothing read
+
+        #--------- PIPE -----------
+        if command[0] == "pipe":
+            pipe()
+
+        #--------- CAT ------------
+        elif command[0] == "wc":
+            wc(command)
         
         #--------- LS -------------
-        if command[0] == 'ls':
-            if len(command) > 1:
-                print("ls: No such file or directory, command not recognized.")
-            list_files()
+        elif command[0] == 'ls': 
+            list_files(command)
 
         #--------- EXIT ----------
         elif command[0] == "exit": #My shell terminates
